4dc74213b1c3d5b975448c8c073ec586388230d9
Justin catello <justin@bigscoots.com>
2024-07-05 08:58:41 -0400

    Update wp-rocket-no-cache.php

--- cache/wp-rocket-no-cache/wp-rocket-no-cache.php
+++ cache/wp-rocket-no-cache/wp-rocket-no-cache.php
@@ -14,48 +14,51 @@
 namespace WP_Rocket\Helpers\cache\no_cache;
 
 // Standard plugin security, keep this line in place.
-defined( 'ABSPATH' ) or die();
+defined('ABSPATH') or die();
 
 /**
  * Disable page caching in WP Rocket.
  * BigScoots - e7iL48AQkzh
  * @link http://docs.wp-rocket.me/article/61-disable-page-caching
  */
-add_filter( 'do_rocket_generate_caching_files', '__return_false' );
-add_filter( 'rocket_generate_advanced_cache_file', '__return_false' );
-add_filter( 'rocket_disable_htaccess', '__return_false' );
+add_filter('do_rocket_generate_caching_files', '__return_false');
+add_filter('rocket_generate_advanced_cache_file', '__return_false');
+add_filter('rocket_disable_htaccess', '__return_false');
 
 
 namespace WP_Rocket\Helpers\static_files\preload\change_parameters;
 
-defined( 'ABSPATH' ) or die();
-   
-function preload_batch_size( $value ) {     
-      
-     $value = 10; 
-   
-     return $value;
- }
- add_filter( 'rocket_preload_cache_pending_jobs_cron_rows_count', __NAMESPACE__ .'\preload_batch_size'  );
+defined('ABSPATH') or die();
 
-function preload_cron_interval( $interval ) {   
-     
-     // change this value, default is 60 seconds:
-     $interval = 120;
-     
-     return $interval;
- }
-add_filter( 'rocket_preload_pending_jobs_cron_interval', __NAMESPACE__ .'\preload_cron_interval'  );
+function preload_batch_size($value)
+{
 
- function preload_requests_delay( $delay_between ) {   
-     
-     // Edit this value, change the number of seconds
-     $seconds = 2.5;
-     // finish editing
-     
-     // All done, don't change this part. 
-     $delay_between = $seconds * 1000000;
-     
-     return $delay_between;
- }
-add_filter( 'rocket_preload_delay_between_requests', __NAMESPACE__ .'\preload_requests_delay'  );
+  $value = 10;
+
+  return $value;
+}
+add_filter('rocket_preload_cache_pending_jobs_cron_rows_count', __NAMESPACE__ . '\preload_batch_size');
+
+function preload_cron_interval($interval)
+{
+
+  // change this value, default is 60 seconds:
+  $interval = 120;
+
+  return $interval;
+}
+add_filter('rocket_preload_pending_jobs_cron_interval', __NAMESPACE__ . '\preload_cron_interval');
+
+function preload_requests_delay($delay_between)
+{
+
+  // Edit this value, change the number of seconds
+  $seconds = 3;
+  // finish editing
+
+  // All done, don't change this part. 
+  $delay_between = $seconds * 1000000;
+
+  return $delay_between;
+}
+add_filter('rocket_preload_delay_between_requests', __NAMESPACE__ . '\preload_requests_delay');
